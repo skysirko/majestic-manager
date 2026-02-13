79bf210300a5b21ed38b27fff80684024c9d93bb
sky.sirko <sky.sirko@gmail.com>
2026-02-13 14:17:33 +0700

    work in progress

--- runcam/majestic_config.c
+++ runcam/majestic_config.c
@@ -1,7 +1,9 @@
 #include "majestic_config.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct majestic_config_data g_majestic_config = {0};
 
@@ -14,9 +16,40 @@ bool majestic_config_init(void) {
 
     char *line = NULL;
     size_t linecap = 0;
+    char section[256] = {0};
+    ssize_t linelen = 0;
 
-    while (getline(&line, &linecap, fp) != -1) {
-        /* no-op */
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
+        if (linelen == 0) {
+            fprintf(stderr, "Empty lines in %s not allowed\n", MAJESTIC_DEFAULT_CONFIG_PATH);
+            free(line);
+            fclose(fp);
+            return false;
+        }
+
+        while (linelen > 0) {
+            char tail = line[linelen - 1];
+            if (tail != '\n' && tail != '\r') {
+                break;
+            }
+            line[--linelen] = '\0';
+        }
+
+        if (linelen == 0) {
+            fprintf(stderr, "Empty lines in %s not allowed\n", MAJESTIC_DEFAULT_CONFIG_PATH);
+            free(line);
+            fclose(fp);
+            return false;
+        }
+
+        if (linelen >= 2 && line[0] == ' ' && line[1] == ' ') {
+            const char *value = line + 2;
+            while (*value == ' ') {
+                value++;
+            }
+            strncpy(section, value, sizeof(section) - 1);
+            section[sizeof(section) - 1] = '\0';
+        }
     }
 
     free(line);
